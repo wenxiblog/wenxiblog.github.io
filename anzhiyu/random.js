0558ab8ca5895509b3f81aa81ba3e7d8aeef3583
wenxiblog <nnceng240@gmail.com>
2025-04-28 21:15:51 +0800

    Site updated: 2025-04-28 21:15:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/28/hello-world/","2025/04/28/这是一篇测试的博文/"];function toRandomPost(){
+var posts=["2025/04/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
