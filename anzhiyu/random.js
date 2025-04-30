9ba70055e12545d1f0ab48a14d68eaa9eb394a3f
wenxiblog <nnceng240@gmail.com>
2025-04-30 12:43:43 +0800

    Site updated: 2025-04-30 12:43:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/29/流量卡推荐/","2025/04/28/这是一篇测试的博文/"];function toRandomPost(){
+var posts=["2025/04/28/这是一篇测试的博文/","2025/04/29/流量卡推荐/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
